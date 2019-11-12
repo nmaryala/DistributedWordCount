79f09659bf8e4420683ee8a3b818aee6838bbdb0
omarkhursheed <omarkhu@gmail.com>
2019-11-11 20:58:21 -0500

    adde wordcount codde

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -19,7 +19,7 @@ public class Client
 			InetAddress ip = InetAddress.getByName("localhost"); 
 	
 			// establish the connection with server port 5056 
-			Socket s = new Socket(ip, 5056); 
+			Socket s = new Socket(ip, 5003); 
 	
 			// obtaining input and out streams 
 			DataInputStream dis = new DataInputStream(s.getInputStream()); 
@@ -27,23 +27,25 @@ public class Client
 	
 			// the following loop performs the exchange of 
 			// information between client and client handler 
+
 			while (true)
 			{ 
 				String masterResponse = dis.readUTF();
 				System.out.println(masterResponse);
-				
-				String filename = "../../test/resources/"+masterResponse;
+				// System.out.println("hey nikhil-0");
+				String filename = "/Users/omarkhursheed/Desktop/CO532/project-2-group-8/src/test/resources/"+masterResponse;
 				BufferedReader reader = new BufferedReader(new FileReader(filename));
+
 				StringBuilder builder = new StringBuilder();
+				// System.out.println("hey nikhil-0");
+				// System.out.println(filename);
 				String currentLine = reader.readLine();
 				while (currentLine != null) {
 			        builder.append(currentLine);
-			        builder.append("n");
 			        currentLine = reader.readLine();
 			    }
 			    reader.close();
-
-				String fileContent = builder.toString();
+			    String fileContent = builder.toString();
 				String[] result = fileContent.split(" ");
 				Map<String, Integer> map = new HashMap<String, Integer> ();
 				for (String t:result) {   
@@ -56,16 +58,17 @@ public class Client
 				    }
 				 }
 				BufferedWriter writer = new BufferedWriter(new FileWriter("temp/samplefile1word" + args[0] + ".txt",true));
-				//map.forEach((key, value) -> System.out.println(key + ":" + value));
+				// map.forEach((key, value) -> System.out.println(key + ":" + value));
 				for (String key : map.keySet()) {
 			        writer.write(key + ":" + map.get(key));
 			        writer.newLine();
 			        writer.flush();
+			    	// dos.writeUTF(key + ":" + map.get(key))
 			    }
 				writer.close();
 
 
-				dos.writeUTF("Done");
+				// dos.writeUTF("Done");
 
 				if(masterResponse == "Over"){
 					break;
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,6 +3,10 @@ import java.util.*;
 import java.net.*;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 // Server class 
 public class Server {
@@ -24,7 +28,7 @@ public class Server {
 		scn.close();
 
 		// server is listening on port 5056
-		ServerSocket ss = new ServerSocket(5056);
+		ServerSocket ss = new ServerSocket(5003);
 
 		// create a new thread object
 		try{
@@ -139,7 +143,41 @@ class ClientHandler extends Thread
 					this.inputQueue.add(nextFileNumber);
 					nextFileNumber = null;
 				}
+
+				HashMap<String, Integer> map1 = new HashMap<String, Integer>();
+
+			    String line;
+			    List<String> filenames = Arrays.asList("temp/samplefile1word1.txt", "temp/samplefile1word2.txt");  
+			    for (int i = 0; i < filenames.size(); i++){
+			    	BufferedReader reader1 = new BufferedReader(new FileReader(filenames.get(i)));
+			    while ((line = reader1.readLine()) != null)
+			    {
+			        String[] parts = line.split(":", 2);
+
+			        if (!map1.containsKey(parts[0]))
+			        {
+			            String key = parts[0];
+			            int value = Integer.parseInt(parts[1]);
+			            map1.put(key, value);
+			        } else {
+			            int count = map1.get(parts[0]);
+					     map1.put(parts[0], count + Integer.parseInt(parts[1]) );
+			        }
+			    }
 				
+				reader1.close();
+	
+			    }
+			  
+ 
+				BufferedWriter writer = new BufferedWriter(new FileWriter("temp/result.txt"));
+				// map.forEach((key, value) -> System.out.println(key + ":" + value));
+				for (String key : map1.keySet()) {
+			        writer.write(key + ":" + map1.get(key)+"\n");
+			        writer.flush();
+			    	// dos.writeUTF(key + ":" + map.get(key))
+			    }
+				writer.close();								
 			} catch (IOException e) { 
 				// e.printStackTrace(); 
 			} 
